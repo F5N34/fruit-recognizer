a3308335151d6ca48c522b87964662effd46fd55
unknown <n.jacques@outlook.fr>
2020-11-24 16:57:22 +0100

    Upload page Cors

---
+++ src/components/MainUpload.vue
@@ -0,0 +1,37 @@
+<template>
+    <dropzone id="foo" ref="el"  :options="options" ></dropzone>
+</template>
+
+<script>
+    import Dropzone from 'nuxt-dropzone'
+    import 'nuxt-dropzone/dropzone.css'
+    export default {
+
+        name: "MainUpload",
+        components:{
+            Dropzone
+        },
+        data(){
+            return{
+               options: {
+                    url: 'https://fruit-recognizer.herokuapp.com/upload',
+                   maxFiles:5,
+            headers:{ 'Cache-Control': 'null',
+                'X-Requested-With': 'null',
+                'Access-Control-Allow-Origin':'*'}
+
+
+
+                },
+
+            }
+        },
+        mounted() {
+            const instance = this.$refs.el.dropzone
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
